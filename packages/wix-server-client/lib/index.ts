1bac4e862e90f7f1d2a3f235ee6e33c0be6702fc
yoav <yoavaa@gmail.com>
2026-01-04 22:10:08 +0200

    added missing exports

--- packages/wix-server-client/lib/index.ts
+++ packages/wix-server-client/lib/index.ts
@@ -1,9 +1,12 @@
 // Export config-related types and functions
 export { loadConfig, type WixConfig, type ApiKeyConfig, type OAuthConfig } from "./config-loader.js";
 
-export {WIX_CLIENT_SERVICE, type WixClientService} from "./wix-client-service";
+export {WIX_CLIENT_SERVICE, type WixClientService} from "./wix-client-service.js";
 
-// Export init and client context
+// Export client context (for dependent plugins to use)
+export { WIX_CLIENT_CONTEXT, type WixClientContext } from './wix-client-context.js';
+
+// Export init
 export { 
     init,
 } from './init.js';
