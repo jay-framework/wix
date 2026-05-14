cfa6160078e7085ffb17358381069ce9cdd64cae
yoav <yoavaa@gmail.com>
2026-05-14 23:45:11 +0300

    v0.17.2

--- packages/wix-stores-v1/lib/contracts/category-list.jay-contract.d.ts
+++ packages/wix-stores-v1/lib/contracts/category-list.jay-contract.d.ts
@@ -1,18 +1,19 @@
-import { HTMLElementCollectionProxy, JayContract } from '@jay-framework/runtime';
+import {HTMLElementCollectionProxy, JayContract} from "@jay-framework/runtime";
+
 
 export interface CategoryOfCategoryListViewState {
-    _id: string;
-    name: string;
-    slug: string;
-    description: string;
-    productCount: number;
-    imageUrl: string;
-    hasImage: boolean;
+  _id: string,
+  name: string,
+  slug: string,
+  description: string,
+  productCount: number,
+  imageUrl: string,
+  hasImage: boolean
 }
 
 export interface CategoryListViewState {
-    categories: Array<CategoryOfCategoryListViewState>;
-    hasCategories: boolean;
+  categories: Array<CategoryOfCategoryListViewState>,
+  hasCategories: boolean
 }
 
 export type CategoryListSlowViewState = Pick<CategoryListViewState, 'hasCategories'> & {
@@ -23,28 +24,18 @@ export type CategoryListFastViewState = {};
 
 export type CategoryListInteractiveViewState = {};
 
+
 export interface CategoryListRefs {
-    categories: {
-        categoryLink: HTMLElementCollectionProxy<
-            CategoryOfCategoryListViewState,
-            HTMLAnchorElement
-        >;
-    };
+  categories: {
+    categoryLink: HTMLElementCollectionProxy<CategoryOfCategoryListViewState, HTMLAnchorElement>
+  }
 }
+
 
 export interface CategoryListRepeatedRefs {
-    categories: {
-        categoryLink: HTMLElementCollectionProxy<
-            CategoryOfCategoryListViewState,
-            HTMLAnchorElement
-        >;
-    };
+  categories: {
+    categoryLink: HTMLElementCollectionProxy<CategoryOfCategoryListViewState, HTMLAnchorElement>
+  }
 }
 
-export type CategoryListContract = JayContract<
-    CategoryListViewState,
-    CategoryListRefs,
-    CategoryListSlowViewState,
-    CategoryListFastViewState,
-    CategoryListInteractiveViewState
->;
+export type CategoryListContract = JayContract<CategoryListViewState, CategoryListRefs, CategoryListSlowViewState, CategoryListFastViewState, CategoryListInteractiveViewState>
--- packages/wix-stores/lib/actions/get-categories.jay-action.d.ts
+++ packages/wix-stores/lib/actions/get-categories.jay-action.d.ts
@@ -3,4 +3,4 @@ export interface GetCategoriesInput {}
 export type GetCategoriesOutput = Array<{
     categoryId: string;
     categoryName: string;
-}>;
+  }>;
